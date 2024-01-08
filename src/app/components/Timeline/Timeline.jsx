4ce5cdbeca2d669fd945866df424e5e9417f98c8
Fabian Yate <fabianyate31@gmail.com>
2024-01-07 22:09:51 -0500

    Added taskList CSS rules in TimelineItem styles
    CSS rules have been set for taskList and its nested elements inside TimelineItem/styles.module.css. With this change, taskList gets a left margin and list items become disc-styled. Additionally, span text within list items gets a specific color, improving visualization.

--- src/app/components/Timeline/Timeline.jsx
+++ src/app/components/Timeline/Timeline.jsx
@@ -5,7 +5,8 @@ const Section = ({items}) => {
   return (
     <ol className={styles.list}>
       {items.map((item, index) => (
-        <TimelineItem key={index} title={item.title} description={item.description} date={item.date}/>
+        <TimelineItem key={index} title={item.title} description={item.description} date={item.date}
+                      tasks={item.tasks}/>
       ))}
     </ol>
   )
--- src/app/components/TimelineItem/TimelineItem.jsx
+++ src/app/components/TimelineItem/TimelineItem.jsx
@@ -1,14 +1,39 @@
 import styles from './styles.module.css'
 
-const TimelineItem = ({date, title, description}) => {
+const TimelineItem = ({date, title, description, tasks}) => {
+  const splitItem = (item) => {
+    return item.split(':');
+  }
+
+  const getTaskTitle = (itemParts) => {
+    return itemParts[0];
+  }
+
+  const getTaskDescription = (itemParts) => {
+    if (itemParts.length > 1) {
+      itemParts.shift();
+      return itemParts.join(':').trim();
+    }
+    return itemParts.join(':');
+  }
+
   return (
     <li className={styles.listItem}>
       <div></div>
       <time>{date}</time>
       <h3>{title}</h3>
       <p>{description}</p>
+      <ul className={styles.tasksList}>
+        {tasks.map((task, index) => {
+          const taskParts = splitItem(task.item);
+          return(
+            <li key={index}>
+              <p><span>{getTaskTitle(taskParts)}: </span>{getTaskDescription(taskParts)}</p>
+            </li>
+          );
+        })}
+      </ul>
     </li>
   )
 };
-
 export default TimelineItem;
