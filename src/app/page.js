46bcea8ec77610ee94c9be00515e5dccca3e6584
Fabian Yate <fabianyate31@gmail.com>
2024-01-08 11:34:45 -0500

    Add PostgreSQL and Postman icons; refactor skills structure
    Refactored the `skills` array in `data.js` into two distinct arrays named `frontendSkills` and `backendSkills`. Also added PostgreSQL and Postman SVG icons. This commit enhances icon collection and improves efficiency in data handling and presentation by categorizing skills according to usage area.

--- src/app/page.js
+++ src/app/page.js
@@ -1,14 +1,15 @@
-import Header from '@/app/components/Header'
-import styles from './page.module.css'
-import picture from '../assets/fabian.webp'
-import Image from "next/image";
-import {projects, skills, socialLinks, workExperiences} from "@/app/data";
+import Card from "@/app/components/Card";
+import Header from '@/app/components/Header';
+import Input from "@/app/components/Input";
 import Pill from "@/app/components/Pill";
 import Section from "@/app/components/Section";
-import {CodeIcon, ContactIcon, SkillsIcon, SuiteCaseIcon} from "@/assets/icons";
 import Timeline from "@/app/components/Timeline";
-import Card from "@/app/components/Card";
-import Input from "@/app/components/Input";
+import { backendSkills, frontendSkills, projects, socialLinks, workExperiences } from "@/app/data";
+import { CodeIcon, ContactIcon, SkillsIcon, SuiteCaseIcon } from "@/assets/icons";
+import Image from "next/image";
+import picture from '../assets/fabian.webp';
+import styles from './page.module.css';
+import Slider from "./components/Slider";
 
 export default function Home() {
   return (
@@ -16,16 +17,16 @@ export default function Home() {
       <div
         className={styles.bg}>
       </div>
-      <Header/>
+      <Header />
       <main className={styles.main}>
         <section id="sobremi" className={styles.aboutSection}>
           <header>
-            <Image className={styles.image} src={picture} alt="Foto de Fabian Yate" loading="eager"/>
+            <Image className={styles.image} src={picture} alt="Foto de Fabian Yate" loading="eager" />
             <h1 className={styles.name}>Hola, soy Fabian Yate</h1>
           </header>
           <p className={styles.description}>
             <span>Desarrollador Full-Stack</span> con +2 años de experiencia. Habilidades en <span>HTML, CSS, JavaScript y
-            React</span> para el
+              React</span> para el
             front-end, y <span>Java con Spring Boot</span> para el back-end.
             Conocimientos en bases de datos y API
             RESTful, enfocado
@@ -35,58 +36,37 @@ export default function Home() {
             {
               socialLinks.map((socialLink, index) => (
                 <Pill key={index} title={socialLink.name} url={socialLink.url}>
-                  <socialLink.Icon/>
+                  <socialLink.Icon />
                 </Pill>
               ))
             }
           </div>
         </section>
 
-        <Section id="experiencia" title="Experiencia Laboral" icon={<SuiteCaseIcon/>}>
-          <Timeline items={workExperiences}/>
+        <Section id="experiencia" title="Experiencia Laboral" icon={<SuiteCaseIcon />}>
+          <Timeline items={workExperiences} />
         </Section>
 
-        <Section id="proyectos" title="Proyectos" icon={<CodeIcon/>}>
+        <Section id="proyectos" title="Proyectos" icon={<CodeIcon />}>
           <div className={styles.bemto}>
             {
               projects.map((project, index) => (
-                <Card key={index} project={project}/>
+                <Card key={index} project={project} />
               ))
             }
           </div>
           <a className={styles.link} href="https://github.com/fabianyater?tab=repositories" target="_blank"
-             style={{width: '120px', margin: '0 auto', marginTop: '40px'}}>Ver más</a>
+            style={{ width: '120px', margin: '0 auto', marginTop: '40px' }}>Ver más</a>
         </Section>
 
-        <Section id="habilidades" title="Habilidades" icon={<SkillsIcon/>}>
+        <Section id="habilidades" title="Habilidades" icon={<SkillsIcon />}>
           <div className={styles.skillsContainer}>
-            <div className={styles.skillsAnimate} data-reverse={false}>
-              {
-                skills.map((skill, index) => (
-                  <div key={index} className={styles.skills}>
-                    <skill.Icon />
-                    <p>{skill.name}</p>
-                  </div>
-                ))
-              }
-            </div>
-            <div className={styles.skillsAnimate} data-reverse={true}>
-              {
-  skills.slice().reverse().map((skill, index) => (
-    <div key={index} className={styles.skills}>
-      <skill.Icon />
-      <p>{skill.name}</p>
-    </div>
-  ))
-}
-
-            </div>
-            <div className={styles.rightShadow}></div>
-            <div className={styles.leftShadow}></div>
+            <Slider data={[...frontendSkills, ...frontendSkills]} reverse={false} />
+            <Slider data={[...backendSkills, ...backendSkills]} reverse />
           </div>
         </Section>
 
-        <Section id="contacto" title="Contáctame" icon={<ContactIcon/>}>
+        <Section id="contacto" title="Contáctame" icon={<ContactIcon />}>
           <section className={styles.contact}>
             <div>
               <h3 className={styles.title}>Colaboremos para <span>crear</span> algo excepcional</h3>
@@ -96,9 +76,9 @@ export default function Home() {
               </p>
             </div>
             <form className={styles.form}>
-              <Input type="text" name="fullname" placeholder="Nombre completo"/>
-              <Input type="email" name="email" placeholder="Correo"/>
-              <textarea rows={8} placeholder="Descripción"/>
+              <Input type="text" name="fullname" placeholder="Nombre completo" />
+              <Input type="email" name="email" placeholder="Correo" />
+              <textarea rows={8} placeholder="Descripción" />
               <button type="submit" disabled title="En desarrollo...">En desarrollo...</button>
             </form>
           </section>
