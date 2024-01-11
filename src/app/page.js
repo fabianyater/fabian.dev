e8045d6a78bd5056fc25b81a72ecfcf556c7a0f3
Fabian Yate <fabianyate31@gmail.com>
2024-01-11 15:49:41 -0500

    Add aboutSection in Section component

--- src/app/page.js
+++ src/app/page.js
@@ -17,7 +17,7 @@ import Slider from "@/app/components/Slider";
 import Form from "@/app/components/Form";
 
 function Home() {
-  const {theme} = useTheme();
+  const { theme } = useTheme();
 
   useEffect(() => {
     document.body.setAttribute("data-theme", theme);
@@ -39,49 +39,50 @@ function Home() {
 
       <Header />
       <main className={styles.main}>
-        <section id="sobremi" className={styles.aboutSection}>
-          <header>
-            <Image className={styles.image} src={picture} alt="Foto de Fabian Yate" loading="eager"/>
-          </header>
-          <p className={styles.description}>
-            <span>Desarrollador Full-Stack</span> con +2 años de experiencia. Habilidades en <span>HTML, CSS, JavaScript y
-              React</span> para el
-            front-end, y <span>Java con Spring Boot</span> para el back-end.
-            Conocimientos en bases de datos y API
-            RESTful, enfocado
-            en crear soluciones efectivas para aplicaciones web dinámicas.
-          </p>
-          <div className={styles.socialLinksContainer}>
-            {
-              socialLinks.map((socialLink, index) => (
-                <Pill key={index} title={socialLink.name} url={socialLink.url}>
-                  <socialLink.Icon/>
-                </Pill>
-              ))
-            }
-          </div>
-        </section>
+        <Section id="sobremi" marginTop="-80px">
+          <section className={styles.aboutSection}>
+            <header>
+              <Image className={styles.image} src={picture} alt="Foto de Fabian Yate" loading="eager" />
+              <h1 className={styles.name}>Hola, soy Fabian Yate</h1>
+            </header>
+            <p className={styles.description}>
+              <span>Desarrollador Full-Stack</span> con +2 años de experiencia. Habilidades en <span>HTML, CSS, JavaScript y
+                React</span> para el
+              front-end, y <span>Java con Spring Boot</span> para el back-end.
+              Conocimientos en bases de datos y API
+              RESTful, enfocado
+              en crear soluciones efectivas para aplicaciones web dinámicas.
+            </p>
+            <div className={styles.socialLinksContainer}>
+              {
+                socialLinks.map((socialLink, index) => (
+                  <Pill key={index} title={socialLink.name} url={socialLink.url}>
+                    <socialLink.Icon />
+                  </Pill>
+                ))
+              }
+            </div>
+          </section>
+        </Section>
 
         <Section id="experiencia" title="Experiencia Laboral" icon={<SuiteCaseIcon />}>
-          <Timeline items={workExperiences}/>
+          <Timeline items={workExperiences} />
         </Section>
 
         <Section id="proyectos" title="Proyectos" icon={<CodeIcon />}>
           <div className={styles.bemto}>
             {
               projects.map((project, index) => (
-                <Card key={index} project={project}/>
+                <Card key={index} project={project} />
               ))
             }
           </div>
-          <a className={styles.link} href="https://github.com/fabianyater?tab=repositories" target="_blank"
-             style={{width: '120px', margin: '0 auto', marginTop: '40px'}}>Ver más</a>
         </Section>
 
         <Section id="habilidades" title="Habilidades" icon={<SkillsIcon />}>
           <div className={styles.skillsContainer}>
-            <Slider data={[...skills.frontendSkills, ...skills.frontendSkills]} reverse={false}/>
-            <Slider data={[...skills.backendSkills, ...skills.backendSkills]} reverse/>
+            <Slider data={[...skills.frontendSkills, ...skills.frontendSkills]} reverse={false} />
+            <Slider data={[...skills.backendSkills, ...skills.backendSkills]} reverse />
             <div className={styles.rightShadow}></div>
             <div className={styles.leftShadow}></div>
           </div>
