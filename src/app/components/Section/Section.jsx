59627285092390736416220c1cc2b83c16cf67ca
Fabian Yate <fabianyate31@gmail.com>
2024-01-11 15:50:05 -0500

    Add custom margin prop and observer to active a section

--- src/app/components/Section/Section.jsx
+++ src/app/components/Section/Section.jsx
@@ -1,10 +1,32 @@
 import {useTheme} from "@/app/context/ThemeProvider";
 import styles from './styles.module.css'
-const Section = ({id, title, icon, children}) => {
+import { useActiveSection } from "@/app/context/ActiveSectionProvider";
+import { useEffect } from "react";
+const Section = ({id, title, icon, children, marginTop}) => {
   const { theme } = useTheme()
+  const { setActiveSection } = useActiveSection()
+
+  useEffect(() => {
+    const section = document.getElementById(id);
+    const observer = new IntersectionObserver(
+      (entries) => {
+        if (entries[0].isIntersecting) {
+          setActiveSection(id);
+          window.history.pushState(null, null, `#${id}`);
+        }
+      },
+      { threshold: 0.5 } // Ajusta este valor según tus necesidades
+    );
+
+    if (section) observer.observe(section);
+
+    return () => {
+      if (section) observer.unobserve(section);
+    };
+  }, [id, setActiveSection]);
 
   return (
-    <section id={id} className={styles.section}>
+    <section id={id} className={styles.section} style={{marginTop: marginTop}}>
       <header className={styles.header} data-theme={theme}>
         {icon && icon}
         <h2>{title}</h2>
